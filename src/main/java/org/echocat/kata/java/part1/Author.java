007f27ecc9e5fbc0da467eb34c969c3adab62ca8
Slavka Tsankova <smtsankova-ext@wunder.tax>
2021-01-03 20:38:16 +0200

    Add Author constructor

--- src/main/java/org/echocat/kata/java/part1/Author.java
+++ src/main/java/org/echocat/kata/java/part1/Author.java
@@ -1,2 +1,13 @@
-package org.echocat.kata.java.part1;public class Author {
+package org.echocat.kata.java.part1;
+
+public class Author {
+	private String email;
+    private String firstName;
+    private String lastName;
+
+	public Author(String email, String firstName, String lastName){
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
 }
