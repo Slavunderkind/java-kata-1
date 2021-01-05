3268b86180fce8b316314b712551fad9524c09f0
Slavka Tsankova <smcankova@gmail.com>
2021-01-05 09:42:53 +0200

    Add PaperRepository and refactor mainApp

--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -1,66 +1,59 @@
 package org.echocat.kata.java.part1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MainApp {
 
-    private final AuthorRepository authorRepository = AuthorRepository.load();
-    private final PaperRepository paperRepository = PaperRepository.load();
+	private final AuthorRepository authorRepository = AuthorRepository.load();
+	private final PaperRepository paperRepository = PaperRepository.load();
 
-    public void run() {
-        authorRepository.findBy("foo@bar.com")
-                .ifPresent(author -> System.out.println("Author: " + author));
-        System.out.println("Here is a list of available actions:");
-        System.out.println("Enter 1 to print all Books and Magazines.");
-        System.out.println("Enter 2 to search by entered ISBN in all Books and Magazines.");
-        System.out.println("Enter 3 to search by entered author email in all Books and Magazines.");
-        System.out.println("Enter 4 to print all Books and Magazines sorted by title.");
+	public void run() {
+		loadInitialData();
+		authorRepository.findBy("foo@bar.com")
+				.ifPresent(author -> System.out.println("Author: " + author));
+		System.out.println("Here is a list of available actions:");
+		System.out.println("Enter 1 to print all Books and Magazines.");
+		System.out.println("Enter 2 to search by entered ISBN in all Books and Magazines.");
+		System.out.println("Enter 3 to search by entered author email in all Books and Magazines.");
+		System.out.println("Enter 4 to print all Books and Magazines sorted by title.");
 
-        System.out.println("Enter an Action: ");
-        Scanner scanner = new Scanner(System.in);
-        String action = scanner.next();
-        pickAnAction(action, scanner);
+		System.out.println("Enter an Action: ");
+		Scanner scanner = new Scanner(System.in);
+		String action = scanner.next();
+		pickAnAction(action, scanner);
 
-    }
+	}
 
-    public static void main(String[] args) {
-        // loadInitialData(); TODO! should be done above ;-)
-        final var app = new MainApp();
-        app.run();
-    }
+	public static void main(String[] args) {
+		final var app = new MainApp();
+		app.run();
+	}
 
-    public static void pickAnAction(String action, Scanner scanner){
-        switch (action) {
-            case "1":
-                Paper.printAll();
-                break;
-            case "2":
-                System.out.println("Enter ISBN: ");
-                String isbn = scanner.next();
+	public static void pickAnAction(String action, Scanner scanner) {
+		switch (action) {
+			case "1" -> Paper.printAll();
+			case "2" -> {
+				System.out.println("Enter ISBN: ");
+				String isbn = scanner.next();
+				Paper.findAllByIsbn(isbn);
+			}
+			case "3" -> {
+				System.out.println("Enter author email: ");
+				String email = scanner.next();
+				Paper.findAllByAuthorEmail(email);
+			}
+			case "4" -> Paper.printAllSortedByTitle();
+		}
+	}
 
-                Paper.findAllByIsbn(isbn);
-                break;
-            case "3":
-                System.out.println("Enter author email: ");
-                String email = scanner.next();
-                Paper.findAllByAuthorEmail(email);
-                break;
-            case "4":
-                Paper.printAllSortedByTitle();
-                break;
-        }
-    }
+	protected static String getHelloWorldText() {
+		return "Hello World!";
+	}
 
-    protected static String getHelloWorldText() {
-        return "Hello World!";
-    }
-
-    protected static void loadInitialData(){
-        LoadData loader = new LoadData();
-        loader.start("books");
-        loader.start("magazines");
-        loader.start("authors");
-    }
+	protected static void loadInitialData() {
+		LoadData loader = new LoadData();
+		loader.start("books");
+		loader.start("magazines");
+		loader.start("authors");
+	}
 }
---
+++ src/main/java/org/echocat/kata/java/part1/PaperRepository.java
@@ -0,0 +1,7 @@
+package org.echocat.kata.java.part1;
+
+public class PaperRepository {
+	public static PaperRepository load() {
+		throw new UnsupportedOperationException("Please implement me.");
+	}
+}
